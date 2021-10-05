24c9f46091bd3082931b10890d56763bf44bc998
dymafr <erwan1309@hotmail.com>
2021-10-05 12:08:08 +0200

    1 file modified

--- src/app/shared/store/auth.reducer.ts
+++ src/app/shared/store/auth.reducer.ts
@@ -4,6 +4,7 @@ import { User } from '../interfaces/user.interface';
 export interface AuthState {
   user: User | null;
   isLoggedin: boolean | null;
+  error: string | null;
 }
 
 export const authStateKey = '[ auth ] key';
@@ -11,6 +12,7 @@ export const authStateKey = '[ auth ] key';
 export const AUTH_INTIAL_STATE: AuthState = {
   user: null,
   isLoggedin: null,
+  error: null,
 };
 
 export const authReducer = createReducer(AUTH_INTIAL_STATE);
